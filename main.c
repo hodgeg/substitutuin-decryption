918d1e8a86151f589194210bb2ee72179fec1ba6
George hodge <hodge.george@rocketmail.com>
2019-04-29 01:38:02 +0000

    finished pt 2 -THIS IS THE ENCRYPTION NOT DECRYPTION

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
-#include <stdio.ch>
-intmain()
+/*The substitution cipher is used to decode a previously encrypted text where the normal
+    alphabet a-z is replaced with a randomly generated alphabet as supplied.
+        The cipher alphabet supplied is 
+"QWERTYUIOPASDFGHJKLZXCVBNM"
+- Note alphabet is in uppercase as required for all text and code will
+    be implemented to support this.
 
-  printf("yeet")
 
-return  0;
+*/
+#include <stdio.h>
+void substitution(char*, char*);
+
+int main() 
+{
+    
+char cipher[26] ="QWERTYUIOPASDFGHJKLZXCVBNM";
+char message[40] = "PLEASE GET MILK AT THE SHOPS";
+
+substitution(message, cipher);
+
+return 0;
+}
+    
+void substitution(char *message, char *cipher)
+{
+int i=0;
+int j=0;
+
+while(message[i]!= 0)
+{
+    if (message[i]>64 &&message[i]<91)
+    { 
+       while (message[i]!=cipher[j])
+       {
+               j++;   
+        }
+             j=message[i]-65;
+             
+            message[i]=cipher[j];
+    }
+        i++;
+        j=0;
+}
+puts(message);
+}
